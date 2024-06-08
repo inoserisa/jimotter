83f026b057f7cfb2735e1a653b309ed206d21e6b
inoserisa <risa.inose@gmail.com>
2024-06-08 22:09:25 +0900

    タブ調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
-    @questions = @user.questions.order(created_at: :desc).page(params[:page]).per(3)
+    @questions = @user.questions.order(created_at: :desc)
   end
 end
