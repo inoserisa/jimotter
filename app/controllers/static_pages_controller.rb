67d98d4ed24292dbba201b6f805e665269fee6e5
inoserisa <risa.inose@gmail.com>
2024-06-17 20:03:16 +0900

    トップ投稿表示数変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def top
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).includes(:user, :prefecture, :city).order(created_at: :desc)
+    @posts = @q.result(distinct: true).includes(:user, :prefecture, :city).order(created_at: :desc).limit(15)
     @questions = Question.all.includes(:user).order(created_at: :desc)
   end
 
