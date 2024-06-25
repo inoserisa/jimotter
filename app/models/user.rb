9653d203068e203ec1e56eb920ca7c60a2e92bac
inoserisa <risa.inose@gmail.com>
2024-06-25 14:46:26 +0900

    退会機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  acts_as_paranoid
+  default_scope { where(deleted_at: nil) }
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :questions, dependent: :destroy
@@ -10,12 +13,21 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, uniqueness: true
+  validate :email_not_taken_by_deleted_user
 
   def own?(object)
     if object.is_a?(User)
       id == object.id
     else
       id == object&.user_id
+    end
+  end
+
+  private
+  
+  def email_not_taken_by_deleted_user
+    if User.with_deleted.where(email: email).exists?
+      errors.add(:email, 'は使用不可です')
     end
   end
 end
