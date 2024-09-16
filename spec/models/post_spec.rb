a26ee0b9cd5243113f39d3b7ade5e1ae37ba1653
inoserisa <risa.inose@gmail.com>
2024-09-16 23:40:10 +0900

    バリデーションテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @prefecture = Prefecture.create(name: '東京都')
+    @city = City.create(name: '新宿区', prefecture_id: @prefecture.id)
+  end
+
+  describe 'バリデーションチェック' do
+    it 'バリデーションが機能しているか' do
+      user = User.create(name: 'test_user', email: 'user@example.com', password: 'password')
+
+      post = user.posts.new(user_id: user.id, prefecture_id: @prefecture.id, city_id: @city.id, content: 'aaa')
+      expect(post).to be_valid
+      expect(post.errors).to be_empty
+    end
+  end
 end
